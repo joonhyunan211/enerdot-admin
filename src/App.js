a57139c80ab7b77e9dc90f5431da122ed50b8fe4
joonhyunan211 <joon@enerdot.co.kr>
2021-08-19 16:15:13 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Amplify, { API } from 'aws-amplify'
+
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Hello AWS Amplifyd
+          Hello AWS Amplify
       </header>
     </div>
   );
